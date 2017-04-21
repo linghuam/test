3f8c97db376513433c139ffcf06386eac95f7f4a
linghuam <1551827898@qq.com>
2017-04-21 15:21:54 +0800

    c

--- leafletCanvasLayer/js/CanvasTileLayer.js
+++ leafletCanvasLayer/js/CanvasTileLayer.js
@@ -1,13 +1,13 @@
-(function(){
-    
+(function() {
+
     /*
-    * 参考http://www.marinetraffic.com/ 实现思路，
-    * 将当前视图区域绘制成canvas切片，按照每个切片的范围加载船舶
-    * 亲测：能承载2万个目标
-    * 
-    */
+     * 参考http://www.marinetraffic.com/ 实现思路，
+     * 将当前视图区域绘制成canvas切片，按照每个切片的范围加载船舶
+     * 亲测：能承载2万个目标
+     *
+     */
     L.CanvasTileLayer = L.TileLayer.extend({
-        createTile: function(coords){
+        createTile: function(coords) {
             // create a <canvas> element for drawing
             var tile = L.DomUtil.create('canvas', 'leaflet-tile');
             // setup tile width and height according to the options
@@ -16,29 +16,23 @@
             tile.height = size.y;
             // get a canvas context and draw something on it using coords.x, coords.y and coords.z
             var ctx = tile.getContext('2d');
-
             // return the tile so it can be rendered on screen
-            // for(var i=0;i<1000;i++){
-                var url = 'img/target_1.png'; //10*22  
-                var img = new Image();   // Create new img element
-                img.onload = function(){
-                    for(var i=0;i<22;i++){
-                        for(var j=0;j<22;j++){
-                            ctx.drawImage(img, i*10, j*22,10,22);   
-                        }
-                    }                    
-                };
-                img.src = url; // Set source path    
-
-            // }
-
-            // ctx.drawImage(img, 0, 0);
+            var url = 'img/target_1.png'; //10*22
+            var img = new Image(); // Create new img element
+            img.onload = function() {
+                for (var i = 0; i < 22; i++) {
+                    for (var j = 0; j < 22; j++) {
+                        ctx.drawImage(img, i * 10, j * 22, 10, 22);
+                    }
+                }
+            };
+            img.src = url; // Set source path
             return tile;
         }
     });
 
-    L.canvastilelayer = function(url,options){
-        return new  L.CanvasTileLayer(url,options);
+    L.canvastilelayer = function(url, options) {
+        return new L.CanvasTileLayer(url, options);
     }
 
 })();
--- leafletCanvasLayer/js/load.js
+++ leafletCanvasLayer/js/load.js
@@ -9,14 +9,44 @@ var mymap = L.map('map',{
 
 L.tileLayer.GoogleLayer().addTo(mymap);
 
+/*
+* 采用SVG绘制矢量图
+ */
+$.getJSON('data/data.json',function(res){
+	var data = res.msg.data;
+	var options = {stroke:false,color:'#ef0300',fillColor:'#ef0300',fillOpacity:1,radius:4,pane:'markerPane'};
+	var layers = [];
+	console.time("render");
+	for(var i=0,len=data.length;i<len;i++){
+		var obj = data[i];
+		var latlng = L.latLng(obj.lat,obj.lon);
+		options.data = obj.v;
+        var circlemarker = L.circleMarker(latlng,options);
+        circlemarker.on("click",function(e){
+        	alert(e.target.options.data);
+        },this);
+        circlemarker.bindTooltip('shipship');
+        layers.push(circlemarker);
+        // circlemarker.addTo(mymap);
+	}
+	L.featureGroup(layers,{pane:'markerPane'}).addTo(mymap);
+	console.timeEnd("render");
+})
+
+/*
+按区域使用canvas作为切片加载
+ */
 // L.canvastilelayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
 
-var boats = [];
+/*
+船舶例子
+ */
+/*var boats = [];
 for(var i=0;i<5000;i++){
 	var boatMarker = L.boatMarker(mymap.getCenter(), { color: "#f1c40f" });
 	boatMarker.setHeading(30);	
 	boats.push(boatMarker);
 }
 var boatsFeatureGroup = L.featureGroup(boats);
-mymap.addLayer(boatsFeatureGroup);
+mymap.addLayer(boatsFeatureGroup);*/
 
