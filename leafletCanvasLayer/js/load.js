07e87a8606baafe28f9fda16e19f9e82f129957f
linghuam <1551827898@qq.com>
2017-04-21 16:25:21 +0800

    c

--- leafletCanvasLayer/js/load.js
+++ leafletCanvasLayer/js/load.js
@@ -1,16 +1,17 @@
-
-var mymap = L.map('map',{
-	center:[49,116],
-	zoom:6
+var map = L.map('map', {
+    center: [49, 116],
+    zoom: 6
 });
 
 // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-// }).addTo(mymap);
+// }).addTo(map);
 
-L.tileLayer.GoogleLayer().addTo(mymap);
+L.tileLayer.GoogleLayer().addTo(map);
+
 
 /*
-* 采用SVG绘制矢量图
+ * 1 采用SVG绘制矢量图
+ * 用一个svg包裹船舶，性能和交互效果都比较好
  */
 $.getJSON('data/data.json',function(res){
 	var data = res.msg.data;
@@ -27,26 +28,52 @@ $.getJSON('data/data.json',function(res){
         },this);
         circlemarker.bindTooltip('shipship');
         layers.push(circlemarker);
-        // circlemarker.addTo(mymap);
+        // circlemarker.addTo(map);
 	}
-	L.featureGroup(layers,{pane:'markerPane'}).addTo(mymap);
+	L.featureGroup(layers,{pane:'markerPane'}).addTo(map);
 	console.timeEnd("render");
-})
+});
+
 
 /*
-按区域使用canvas作为切片加载
+ * 2 按区域使用canvas作为切片加载
+ * 将船舶批量绘制在canvas上，性能很好，但动态交互实现太复杂
  */
-// L.canvastilelayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
+// L.canvastilelayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
 
 /*
-船舶例子
+ * 3 船舶例子
+ * 每个船舶是一个canvas，性能与使用图片差不多
  */
 /*var boats = [];
 for(var i=0;i<5000;i++){
-	var boatMarker = L.boatMarker(mymap.getCenter(), { color: "#f1c40f" });
+	var boatMarker = L.boatMarker(map.getCenter(), { color: "#f1c40f" });
 	boatMarker.setHeading(30);	
 	boats.push(boatMarker);
 }
 var boatsFeatureGroup = L.featureGroup(boats);
-mymap.addLayer(boatsFeatureGroup);*/
+map.addLayer(boatsFeatureGroup);*/
 
+
+/*
+ * 4 leaflet-vector-markers
+ * 每个船舶是一个svg，性能与使用图片差不多
+ */
+// $.getJSON('data/data.json', function(res) {
+//     var data = res.msg.data;
+//     var layers = [];
+//     console.time("render");
+//     var redMarker = L.VectorMarkers.icon({
+//         icon: 'coffee',
+//         markerColor: 'red'
+//     });
+//     for (var i = 0, len = data.length; i < 5000; i++) {
+//         var obj = data[i];
+//         var latlng = L.latLng(obj.lat, obj.lon);
+//         var marker = L.marker(latlng, { icon: redMarker }).addTo(map);
+//         layers.push(marker);
+//     }
+//     // L.featureGroup(layers, { pane: 'markerPane' }).addTo(map);
+//     console.timeEnd("render");
+// });
