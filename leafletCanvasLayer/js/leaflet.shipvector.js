eb7a68239301ee0894770a861f0604f5ca99e481
linghuam <1551827898@qq.com>
2017-04-24 12:51:58 +0800

    c

--- leafletCanvasLayer/js/leaflet.shipvector.js
+++ leafletCanvasLayer/js/leaflet.shipvector.js
@@ -1,24 +1,42 @@
 L.SVG.include({
-    _updateCircle: function(layer) {
+    _updateShipVector: function(layer) {
         var p = layer._point,
             r = layer._radius,
             r2 = layer._radiusY || r,
-            arc = 'a' + r + ',' + r2 + ' 0 1,0 ';
+            arc = 'a' + r + ',' + r2 + ' 0 1,0 ',
+            r3 = r * Math.pow(3, 1 / 2) / 2,
+            p2 = { x: p.x - r / 2, y: p.y + r3 },
+            p3 = { x: p.x + r / 2, y: p.y + r3 };
 
         // drawing a circle with two half-arcs
         var d = layer._empty() ? 'M0 0' :
-            'M' + (p.x - r) + ',' + p.y +
-            arc + (r * 2) + ',0 ' +
-            arc + (-r * 2) + ',0 ';
+            'M' + p.x + ',' + p.y + ' L' + p2.x + ',' + p2.y + ' L' + p3.x + ',' + p3.y + ' Z';
 
         this._setPath(layer, d);
-    },
-
-    _setPath: function(layer, path) {
-        layer._path.setAttribute('d', path);
     }
+
 });
 
 L.ShipVector = L.CircleMarker.extend({
 
+    options: {
+        rotationAngle: 0, //方向，正北是0，顺时针，共360，
+        renderer: L.svg()
+    },
+
+    setRotationAngle: function(angel) {
+    	if(this._path){
+    		var transform = '';
+            transform += ' rotate(' + angel + 'deg)';
+            this._path.style[L.DomUtil.TRANSFORM] += transform;
+            this._path.style.transformOrigin = "50% 50%"; //linghuam 设置旋转的参照点为中心点，解决船舶偏移问题
+    	}
+    },
+
+    _updatePath: function() {
+        this._renderer._updateShipVector(this);
+        this.setRotationAngle(this.options.rotationAngle);
+    }
 });
+
+
--- leafletCanvasLayer/js/load.js
+++ leafletCanvasLayer/js/load.js
@@ -15,38 +15,38 @@ L.tileLayer.GoogleLayer().addTo(map);
  * renderer:L.canvas()  在canvas上绘制矢量图，效率比svg好，但leaflet对事件支持不太好
  * renderer:L.svg() 在svg上绘制矢量图，对事件支持较好，但效率比canvas差
  */
-$.getJSON('data/data.json', function(res) {
-    var data = res.msg.data;
-    var options = { stroke: false, color: '#ef0300', fillColor: '#ef0300', fillOpacity: 1, radius: 4, pane: 'markerPane', className: 'leaflet-shipmarker' };
-    var layers = [];
-    console.time("render");
-    for (var i = 0, len = data.length; i < len; i++) {
-        var obj = data[i];
+// $.getJSON('data/data.json', function(res) {
+//     var data = res.msg.data;
+//     var options = { stroke: false, color: '#ef0300', fillColor: '#ef0300', fillOpacity: 1, radius: 4, pane: 'markerPane', className: 'leaflet-shipmarker' };
+//     var layers = [];
+//     console.time("render");
+//     for (var i = 0, len = data.length; i < len; i++) {
+//         var obj = data[i];
 
-        var latlng = L.latLng(obj.lat, obj.lon);
-        options.data = obj.v;
-        var circlemarker = L.circleMarker(latlng, options);
-        circlemarker.on("click", function(e) {
-            alert(e.target.options.data);
-        }, this);
-        circlemarker.bindTooltip('shipship');
-        layers.push(circlemarker);
+//         var latlng = L.latLng(obj.lat, obj.lon);
+//         options.data = obj.v;
+//         var circlemarker = L.circleMarker(latlng, options);
+//         circlemarker.on("click", function(e) {
+//             alert(e.target.options.data);
+//         }, this);
+//         circlemarker.bindTooltip('shipship');
+//         layers.push(circlemarker);
 
-        // var latlngbounds = L.latLngBounds(L.latLng(obj.lat,obj.lon),L.latLng(obj.lat+1,obj.lon+1));
-        // layers.push(L.rectangle(latlngbounds));
-    }
-    L.featureGroup(layers).addTo(map);
-    console.timeEnd("render");
+//         // var latlngbounds = L.latLngBounds(L.latLng(obj.lat,obj.lon),L.latLng(obj.lat+1,obj.lon+1));
+//         // layers.push(L.rectangle(latlngbounds));
+//     }
+//     L.featureGroup(layers).addTo(map);
+//     console.timeEnd("render");
 
-    //contextmenu 
-    $.contextMenu({
-        selector: '.leaflet-shipmarker', //选择右键菜单触发的元素
-        items: {
-            foo: { name: "Foo", callback: function(key, opt) { alert("Foo!"); } },
-            bar: { name: "Bar", callback: function(key, opt) { alert("Bar!") } }
-        }
-    });
-});
+//     //contextmenu 
+//     $.contextMenu({
+//         selector: '.leaflet-shipmarker', //选择右键菜单触发的元素
+//         items: {
+//             foo: { name: "Foo", callback: function(key, opt) { alert("Foo!"); } },
+//             bar: { name: "Bar", callback: function(key, opt) { alert("Bar!") } }
+//         }
+//     });
+// });
 
 /*
  * 2 按区域使用canvas作为切片加载
@@ -59,15 +59,14 @@ $.getJSON('data/data.json', function(res) {
  * 3 船舶例子
  * 每个船舶是一个canvas，性能与使用图片差不多
  */
-/*var boats = [];
-for(var i=0;i<5000;i++){
-	var boatMarker = L.boatMarker(map.getCenter(), { color: "#f1c40f" });
-	boatMarker.setHeading(30);	
-	boats.push(boatMarker);
-}
-var boatsFeatureGroup = L.featureGroup(boats);
-map.addLayer(boatsFeatureGroup);*/
-
+// var boats = [];
+// for(var i=0;i<1;i++){
+// 	var boatMarker = L.boatMarker(map.getCenter(), { color: "#f1c40f" });
+// 	boatMarker.setHeading(30);	
+// 	boats.push(boatMarker);
+// }
+// var boatsFeatureGroup = L.featureGroup(boats);
+// map.addLayer(boatsFeatureGroup);
 
 /*
  * 4 leaflet-vector-markers
