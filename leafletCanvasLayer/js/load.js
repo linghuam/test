02ae87e2ed1e1c6e76de0627e1b260363ebc095a
linghuam <1551827898@qq.com>
2017-04-26 14:36:36 +0800

    c

--- leafletCanvasLayer/js/load.js
+++ leafletCanvasLayer/js/load.js
@@ -11,39 +11,39 @@ L.tileLayer.GoogleLayer().addTo(map);
 
 /*
  * 1 采用SVG绘制矢量图  https://developer.mozilla.org/docs/Web/SVG
- * 用一个svg包裹船舶，性能和交互效果都比较好
+ * 用一个svg包裹船舶，容易实现，性能和交互效果比较好。
  * renderer:L.canvas()  在canvas上绘制矢量图，效率比svg好，但leaflet对事件支持不太好
  * renderer:L.svg() 在svg上绘制矢量图，对事件支持较好，但效率比canvas差
  */
-$.getJSON('data/data.json', function(res) {
-    var data = res.msg.data;
-    var options = {width:10,height:22, stroke: false, color: '#ef0300', fillColor: '#ef0300', fillOpacity: 1, radius: 4, pane: 'markerPane', className: 'leaflet-shipmarker' };
-    var layers = [];
-    console.time("render");
-    for (var i = 0, len = data.length; i < len; i++) {
-        var obj = data[i];
+// $.getJSON('data/data.json', function(res) {
+//     var data = res.msg.data;
+//     var options = {width:10,height:22, stroke: false, color: '#ef0300', fillColor: '#ef0300', fillOpacity: 1, radius: 4, pane: 'markerPane', className: 'leaflet-shipmarker' };
+//     var layers = [];
+//     console.time("render");
+//     for (var i = 0, len = data.length; i < len; i++) {
+//         var obj = data[i];
 
-        var latlng = L.latLng(obj.lat, obj.lon);
-        options.data = obj.v;
-        var circlemarker = L.shipVector(latlng, options);
-        circlemarker.on("click", function(e) {
-            alert(e.target.options.data);
-        }, this);
-        circlemarker.bindTooltip('shipship');
-        layers.push(circlemarker);
-    }
-    L.featureGroup(layers).addTo(map);
-    console.timeEnd("render");
+//         var latlng = L.latLng(obj.lat, obj.lon);
+//         options.data = obj.v;
+//         var circlemarker = L.shipVector(latlng, options);
+//         circlemarker.on("click", function(e) {
+//             alert(e.target.options.data);
+//         }, this);
+//         circlemarker.bindTooltip('shipship');
+//         layers.push(circlemarker);
+//     }
+//     L.featureGroup(layers).addTo(map);
+//     console.timeEnd("render");
 
-    //contextmenu 
-    $.contextMenu({
-        selector: '.leaflet-shipmarker', //选择右键菜单触发的元素
-        items: {
-            foo: { name: "Foo", callback: function(key, opt) { alert("Foo!"); } },
-            bar: { name: "Bar", callback: function(key, opt) { alert("Bar!") } }
-        }
-    });
-});
+//     //contextmenu 
+//     $.contextMenu({
+//         selector: '.leaflet-shipmarker', //选择右键菜单触发的元素
+//         items: {
+//             foo: { name: "Foo", callback: function(key, opt) { alert("Foo!"); } },
+//             bar: { name: "Bar", callback: function(key, opt) { alert("Bar!") } }
+//         }
+//     });
+// });
 
 /*
  * 2 按区域使用canvas作为切片加载
