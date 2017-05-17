561b1dda48faea6251125a0e58794d6305ac6c53
linghuam <1551827898@qq.com>
2017-05-17 16:56:32 +0800

    c

--- turf/load.js
+++ turf/load.js
@@ -2,6 +2,10 @@ var map = L.map('map', {
     center: [30, 116],
     zoom: 4
 });
+map.on("mousemove", function(e) {
+    var latlng = e.latlng;
+    $('#msg').text(JSON.stringify(latlng));
+}, this);
 L.tileLayer.GoogleLayer().addTo(map);
 
 // create random points with random
@@ -16,34 +20,87 @@ L.tileLayer.GoogleLayer().addTo(map);
 // var isolined = turf.isolines(points, 'z', 15, breaks);
 // L.geoJSON(isolined).addTo(map);
 
+
+
 // Parse local CSV file
+// Papa.parse('data.csv', {
+//     download: true,
+//     complete: function(results) {
+//         console.log("Finished:", results.data);
+//         var points = {
+//             type: "FeatureCollection",
+//             features: []
+//         };
+//         for (var i = 0, len = results.data.length; i < len; i++) {
+//             var row = results.data[i];
+//             var lat = +row[0];
+//             var lng = +row[1];
+//             var value = +row[2];
+//             var point = {
+//                 type: 'Feature',
+//                 bbox: [-180.0, -90.0, 180.0, 90.0],
+//                 geometry: {
+//                     type: 'Point',
+//                     coordinates: [lng, lat]
+//                 },
+//                 properties: {
+//                     z: value
+//                 }
+//             };
+//             points.features.push(point);
+//         }
+//         var breaks = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7];
+//         var isolined = turf.isolines(points, 'z', 15, breaks);
+//         L.geoJSON(isolined).addTo(map);
+//     }
+// });
+
+
 Papa.parse('data.csv', {
     download: true,
     complete: function(results) {
         console.log("Finished:", results.data);
-        var points = {
-            type: "FeatureCollection",
-            features: []
-        };
-        for (var i = 0, len = results.data.length; i < len; i++) {
-            var row = results.data[i];
-            var lat = +row[0];
-            var lng = +row[1];
-            var value = +row[2];
-            var point = {
-                type: 'Feature',
-                geometry: {
-                    type: 'Point',
-                    coordinates: [lng, lat]
-                },
-                properties: {
-                    z: value
+    },
+    step: function(results, parser) {
+        if (results.data[0][0] === '' && this.arr.length) {
+            // console.log(JSON.stringify(this.arr));
+
+            // handler data -------------start------------------------
+            var latlngs = [];
+            for (var i = 0, len = this.arr.length; i < len; i++) {
+                var row = this.arr[i];
+                var lat = +row[0];
+                var lng = +row[1];
+                var value = +row[2];
+                var latlng = L.latLng(lat, lng);
+
+                if (i === 0) {
+                    latlngs.push(latlng);
+                } else {
+                    var lastlng = (latlngs[latlngs.length - 1]).lng;
+                    var extend = Math.abs(lng - lastlng);
+                    if (extend >= 180) {  //解决经度范围超过180连线异常
+                        L.polyline(latlngs).addTo(map);
+                        latlngs = [];
+                        latlngs.push(latlng);
+                    } else {
+                    	latlngs.push(latlng);
+                        if (i === len - 1) {
+                            L.polyline(latlngs).addTo(map);
+                            latlngs = [];
+                        }
+                    }
                 }
-            };
-            points.features.push(point);
+            }
+            // handler datq ----------------end-----------------------------
+            // 
+            this.arr = [];
+        } else {
+            if (this.arr) {
+                this.arr.push(results.data[0]);
+            } else {
+                this.arr = [];
+            }
         }
-        var breaks = [0, 0.2, 0.4, 0.6,  0.8, 1,1.2,1.4];
-        var isolined = turf.isolines(points, 'z', 15, breaks);
-        L.geoJSON(isolined).addTo(map);
     }
 });
