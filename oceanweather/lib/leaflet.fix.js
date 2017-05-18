00e54e860f44d8e60de0cffebcca8eb95c213dfd
linghuam <1551827898@qq.com>
2017-05-18 17:53:31 +0800

    linqtojs

--- oceanweather/lib/leaflet.fix.js
+++ oceanweather/lib/leaflet.fix.js
@@ -1,7 +1,7 @@
 /*
- * 矩形内写文字 svg
+ * 线写文字 svg
  */
-L.VectorLabelSvg = L.SVG.extend({
+L.TextLineSvg = L.SVG.extend({
 
     _updatePoly: function(layer, closed) {
         L.SVG.prototype._updatePoly.call(this, layer, closed);
@@ -12,10 +12,20 @@ L.VectorLabelSvg = L.SVG.extend({
         }
     },
 
+    _initContainer:function(){
+        L.SVG.prototype._initContainer.call(this);
+        this._defs = L.SVG.create('defs');
+        this._container.appendChild(this._defs);
+    },
+
     _initPath: function(layer) {
         L.SVG.prototype._initPath.call(this, layer);
         if (layer.options.text) {
-            layer._textele = L.SVG.create('text');
+            layer._textele = L.SVG.create('text');   
+            layer._textpath = L.SVG.create('textPath');  
+            layer._textele.appendChild(layer._textpath);
+            layer._textdefpath= L.SVG.create('path'); 
+            layer._textdefpath.setAttribute('id','textpath'+L.stamp({}));     
         }
     },
 
@@ -23,13 +33,15 @@ L.VectorLabelSvg = L.SVG.extend({
         L.SVG.prototype._addPath.call(this, layer);
         if (layer._textele) {
             this._rootGroup.appendChild(layer._textele);
-        }
+            this._defs.appendChild(layer._textdefpath);
+        }        
     },
 
     _removePath: function(layer) {
         L.SVG.prototype._removePath.call(this, layer);
         if (layer._textele) {
             L.DomUtil.remove(layer._textele);
+            L.DomUtil.remove(layer._textdefpath);
         }
     },
 
@@ -37,13 +49,22 @@ L.VectorLabelSvg = L.SVG.extend({
         var container = this._container,
             text = layer.options.text,
             textele = layer._textele,
+            pathd = layer._path.getAttribute('d'),
+            hrefid = layer._textdefpath.getAttribute('id'),
             map = this._map;
-        var center = layer.getBounds().getCenter();
-        var pt = map.latLngToLayerPoint(center);
-        textele.setAttribute('x', pt.x);
-        textele.setAttribute('y', pt.y + 7);
-        textele.setAttribute('text-anchor', 'middle');
-        textele.setAttribute('style', 'font-family:"Microsoft YaHei";font-size:14px;');
-        textele.innerHTML = text;
+        layer._textdefpath.setAttribute('d',pathd);
+        var len = layer._textdefpath.getTotalLength();
+        layer._textele.setAttribute('dx',len/2);
+        layer._textele.setAttribute('dy','2');
+        layer._textpath.setAttribute('href','#'+hrefid);
+        layer._textpath.setAttribute('style','font-size:12px;font-family:Microsoft YaiHei;fill:#0415FA');
+        layer._textpath.innerHTML = text;
+        // var center = layer.getBounds().getCenter();
+        // var pt = map.latLngToLayerPoint(center);
+        // textele.setAttribute('x', pt.x);
+        // textele.setAttribute('y', pt.y + 7);
+        // textele.setAttribute('text-anchor', 'middle');
+        // textele.setAttribute('style', 'font-family:"Microsoft YaHei";font-size:14px;');
+        // textele.innerHTML = text;
     }
 });
--- oceanweather/load.js
+++ oceanweather/load.js
@@ -11,51 +11,60 @@ L.tileLayer.GoogleLayer().addTo(map);
 
 var OceanWeather = {};
 OceanWeather.pressure = function() {
-    PapaParse('data/pressure.csv');
+    PapaParseLine('data/pressure.csv');
+    // PapaParsePressureBand('data/pressurehl.csv');
 }
 OceanWeather.mb500 = function() {
-    PapaParse('data/500mb.csv');
+    PapaParseLine('data/500mb.csv');
 }
 OceanWeather.wind = function() {
     PapaParse('data/wind.csv');
 }
 OceanWeather.waveheight = function() {
-    PapaParse('data/waveheight.csv');
+    PapaParseArea('data/waveheight.csv');
 }
 OceanWeather.surge = function() {
-    PapaParse('data/surge.csv');
+    PapaParseArea('data/surge.csv');
 }
 OceanWeather.flow = function() {
-    PapaParse('data/flow.csv');
+    PapaParseArea('data/flow.csv');
 }
 OceanWeather.temperature = function() {
-    PapaParse('data/temperature.csv');
+    PapaParseArea('data/temperature.csv');
 }
 OceanWeather.visibility = function() {
-    PapaParse('data/visibility.csv');
+    PapaParseArea('data/visibility.csv');
 }
 
 var featueGroup = L.featureGroup([]).addTo(map);
+var renderer = new L.TextLineSvg();
 
-function PapaParse(url) {
+function PapaParseArea(url) {
     featueGroup.clearLayers();
-    this.arr = [];
+    var options = {
+        renderer: renderer,
+        color: '#000',
+        weight: 1,
+        fill: true,
+        fillColor: '#f00'
+    };
+    var arr = [];
     Papa.parse(url, {
         download: true,
         complete: function(results) {
             // console.log("Finished:", results.data);
         },
         step: function(results, parser) {
-            if (results.data[0][0] === '' && this.arr.length) {
-                // console.log(JSON.stringify(this.arr));
+            if (results.data[0][0] === '' && arr.length) {
+                // console.log(JSON.stringify(arr));
 
                 // handler data -------------start------------------------
                 var latlngs = [];
-                for (var i = 0, len = this.arr.length; i < len; i++) {
-                    var row = this.arr[i];
+                for (var i = 0, len = arr.length; i < len; i++) {
+                    var row = arr[i];
                     var lat = +row[0];
                     var lng = +row[1];
-                    var value = +row[2];
+                    var value = options.text = +row[2];
                     var latlng = L.latLng(lat, lng);
 
                     if (i === 0) {
@@ -64,28 +73,164 @@ function PapaParse(url) {
                         var lastlng = (latlngs[latlngs.length - 1]).lng;
                         var extend = Math.abs(lng - lastlng);
                         if (extend >= 180) { //解决经度范围超过180连线异常
-                            featueGroup.addLayer(L.polyline(latlngs));
+                            featueGroup.addLayer(L.polygon(latlngs, options));
                             latlngs = [];
                             latlngs.push(latlng);
                         } else {
                             latlngs.push(latlng);
                             if (i === len - 1) {
-                                featueGroup.addLayer(L.polyline(latlngs));
+                                featueGroup.addLayer(L.polygon(latlngs, options));
                                 latlngs = [];
                             }
                         }
                     }
                 }
                 // handler datq ----------------end-----------------------------
-                
-                this.arr = [];
+
+                arr = [];
             } else {
-                if (this.arr) {
-                    this.arr.push(results.data[0]);
+                if (arr) {
+                    arr.push(results.data[0]);
                 } else {
-                    this.arr = [];
+                    arr = [];
                 }
             }
         }
+    });
+}
+
+function PapaParseLine(url) {
+    featueGroup.clearLayers();
+    var options = {
+        renderer: renderer,
+        color: '#000',
+        weight: 1,
+        fill: false
+    };
+    var arr = [];
+    Papa.parse(url, {
+        download: true,
+        complete: function(results) {
+            // console.log("Finished:", results.data);
+        },
+        step: function(results, parser) {
+            if (results.data[0][0] === '' && arr.length) {
+                // console.log(JSON.stringify(arr));
+
+                // handler data -------------start------------------------
+                var latlngs = [];
+                for (var i = 0, len = arr.length; i < len; i++) {
+                    var row = arr[i];
+                    var lat = +row[0];
+                    var lng = +row[1];
+                    var value = options.text = +row[2];
+                    var latlng = L.latLng(lat, lng);
+
+                    if (i === 0) {
+                        latlngs.push(latlng);
+                    } else {
+                        var lastlng = (latlngs[latlngs.length - 1]).lng;
+                        var extend = Math.abs(lng - lastlng);
+                        if (extend >= 180) { //解决经度范围超过180连线异常
+                            featueGroup.addLayer(L.polyline(latlngs, options));
+                            latlngs = [];
+                            latlngs.push(latlng);
+                        } else {
+                            latlngs.push(latlng);
+                            if (i === len - 1) {
+                                featueGroup.addLayer(L.polyline(latlngs, options));
+                                latlngs = [];
+                            }
+                        }
+                    }
+                }
+                // handler datq ----------------end-----------------------------
+
+                arr = [];
+            } else {
+                if (arr) {
+                    arr.push(results.data[0]);
+                } else {
+                    arr = [];
+                }
+            }
+        }
+    });
+}
+
+function PapaParsePressureBand(url) {
+   var options = {
+        renderer: renderer,
+        color: '#000',
+        weight: 6,
+        fill: true
+    };
+    var arr = [];
+    Papa.parse(url, {
+        download: true,
+        complete: function(results) {
+            var datas = results.data;
+            var res = datas.groupBy(o=>o[3]);
+            for(var i=0,len=res.length;i<len;i++){
+                var objitem = res[i];
+               options.text = objitem.key;
+                if(objitem.value.length <2){
+                    continue;
+                }
+                var latlngs = [];
+                for(var j=0,lenj=objitem.value.length;j<lenj;j++){
+                    var row = objitem.value[j];
+                    var lat = +row[0];
+                    var lng = +row[1];
+                    var v = +row[2];
+                    v === 0 ?options.fillColor = '#f00' :options.fillColor = '#00f';
+                    var latlng = L.latLng(lat,lng);
+                    latlngs.push(latlng);
+                }
+                featueGroup.addLayer(L.polyline(latlngs, options));
+            }
+        }
+        // step: function(results, parser) {
+            // if (results.data[0][0] === '' && this.arr.length) {
+            //     // console.log(JSON.stringify(this.arr));
+
+            //     // handler data -------------start------------------------
+            //     var latlngs = [];
+            //     for (var i = 0, len = this.arr.length; i < len; i++) {
+            //         var row = this.arr[i];
+            //         var lat = +row[0];
+            //         var lng = +row[1];
+            //         var value = options.text = +row[2];
+            //         var latlng = L.latLng(lat, lng);
+
+            //         if (i === 0) {
+            //             latlngs.push(latlng);
+            //         } else {
+            //             var lastlng = (latlngs[latlngs.length - 1]).lng;
+            //             var extend = Math.abs(lng - lastlng);
+            //             if (extend >= 180) { //解决经度范围超过180连线异常
+            //                 featueGroup.addLayer(L.polyline(latlngs, this.options));
+            //                 latlngs = [];
+            //                 latlngs.push(latlng);
+            //             } else {
+            //                 latlngs.push(latlng);
+            //                 if (i === len - 1) {
+            //                     featueGroup.addLayer(L.polyline(latlngs, this.options));
+            //                     latlngs = [];
+            //                 }
+            //             }
+            //         }
+            //     }
+            //     // handler datq ----------------end-----------------------------
+
+            //     this.arr = [];
+            // } else {
+            //     if (this.arr) {
+            //         this.arr.push(results.data[0]);
+            //     } else {
+            //         this.arr = [];
+            //     }
+            // }
+        // }
     });
 }
