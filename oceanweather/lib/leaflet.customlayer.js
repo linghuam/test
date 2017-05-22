4030f519b3f44380724f307fe21856f9fddd488a
linghuam <1551827898@qq.com>
2017-05-22 18:16:02 +0800

    customelayer

--- oceanweather/lib/leaflet.customlayer.js
+++ oceanweather/lib/leaflet.customlayer.js
@@ -1,30 +1,22 @@
 L.CustomLayer = L.Layer.extend({
-
-    options:{
-        pane:'overlayPane'
-    },
-
-    initialize:function(options,data){
-        L.setOptions(this,options);
-        this._data = data || [];  //{lat:...,lon:....,dir:...,value:...}
-    },
-
     onAdd: function(map) {
         var pane = map.getPane(this.options.pane);
         this._container = L.DomUtil.create('canvas');
+        this._ctx = this._container.getContext('2d');
 
         pane.appendChild(this._container);
 
         // Calculate initial position of container with `L.Map.latLngToLayerPoint()`, `getPixelOrigin()` and/or `getPixelBounds()`
+        // var point = map.getPixelOrigin();
         var size = map.getSize();
+        // L.DomUtil.setPosition(this._container, point);
         this._container.style.width = size.x + 'px';
         this._container.style.height = size.y + 'px';
         this._container.style.position = 'absolute';
-        // L.DomUtil.setPosition(this._container, point);
-
         // Add and position children elements if needed
 
         map.on('zoomend viewreset', this._update, this);
+        this._update();
     },
 
     onRemove: function(map) {
@@ -33,10 +25,15 @@ L.CustomLayer = L.Layer.extend({
     },
 
     _update: function() {
-        // Recalculate position of container
-
-        L.DomUtil.setPosition(this._container, point);        
-
-        // Add/remove/reposition children elements if needed
+        var mapPane = this._map.getPanes().mapPane;
+        var point = mapPane._leaflet_pos;
+        this._container.style.transform = 'translate(' +
+            -Math.round(point.x) + 'px,' +
+            -Math.round(point.y) + 'px)';
+        var ctx = this._ctx;
+        var latlng = L.latLng(30, 116);
+        var pt = this._map.latLngToContainerPoint(latlng);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(pt.x, pt.y, 10, 10);
     }
 });
