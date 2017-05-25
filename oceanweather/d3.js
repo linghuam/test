38a50c5ed6f8bcbc20f83d908f78f94b233359c5
linghuam <1551827898@qq.com>
2017-05-25 16:57:42 +0800

    d3overlay

--- oceanweather/d3.js
+++ oceanweather/d3.js
@@ -1,3 +1,16 @@
-OceanWeather.d3 = function(){
-	alert('d3');
+/*
+ * https://github.com/d3/d3-contour
+ * https://en.wikipedia.org/wiki/Marching_squares
+ * http://blog.csdn.net/silangquan/article/details/47054309
+ */
+OceanWeather.d3 = function() {
+	if(map.hasLayer(this.d3Overlay)){map.removeLayer(this.d3Overlay);}
+    this.d3Overlay = L.d3SvgOverlay(function(selection, projection) {
+    	d3Draw(selection, projection);
+    });
+    map.addLayer(this.d3Overlay);
+};
+
+function d3Draw(selection, projection){
+
 }
