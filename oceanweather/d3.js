9345609b888fea82606abde3e0e767313445edb8
linghuam <1551827898@qq.com>
2017-05-26 18:09:29 +0800

    d3-geo
    Signed-off-by: linghuam <1551827898@qq.com>

--- oceanweather/d3.js
+++ oceanweather/d3.js
@@ -4,13 +4,17 @@
  * http://blog.csdn.net/silangquan/article/details/47054309
  */
 OceanWeather.d3 = function() {
-	if(map.hasLayer(this.d3Overlay)){map.removeLayer(this.d3Overlay);}
+    if (map.hasLayer(this.d3Overlay)) { map.removeLayer(this.d3Overlay); }
     this.d3Overlay = L.d3SvgOverlay(function(selection, projection) {
-    	d3Draw(selection, projection);
+        d3Draw(selection, projection);
     });
     map.addLayer(this.d3Overlay);
 };
 
-function d3Draw(selection, projection){
-
+function d3Draw(selection, projection) {
+    d3.json('data/geojson.json', function(data) {
+        selection.append("path")
+            .datum(data)
+            .attr("d", d3.geoPath());
+    });
 }
