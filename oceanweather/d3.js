f8ecc79a026ce8b5addf8fb121fba6134b0f6096
linghuam <1551827898@qq.com>
2017-05-27 10:48:20 +0800

    d3-drawGeojson
    Signed-off-by: linghuam <1551827898@qq.com>

--- oceanweather/d3.js
+++ oceanweather/d3.js
@@ -7,15 +7,23 @@
 OceanWeather.d3 = function() {
     if (map.hasLayer(this.d3Overlay)) { map.removeLayer(this.d3Overlay); }
     this.d3Overlay = L.d3SvgOverlay(function(selection, projection) {
-        d3Draw(selection, projection);
+        d3DrawChina(selection, projection);
     });
     map.addLayer(this.d3Overlay);
 };
 
-function d3Draw(selection, projection) {
+function d3DrawChina(selection, projection) {
+
     d3.json('data/geojson.json', function(data) {
+
+        var transform = d3.geoTransform({
+            point: function(x, y) {
+            	var pt = projection.latLngToLayerPoint(L.latLng(y,x));
+                this.stream.point(pt.x, pt.y);
+            }
+        });
         selection.append("path")
             .datum(data)
-            .attr("d", d3.geoPath());
+            .attr("d", d3.geoPath().projection(transform));
     });
 }
