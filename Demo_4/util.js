6502eb8837730d9824b1be6b2db970749214a2d2
linghuam <1551827898@qq.com>
2017-08-15 10:36:20 +0800

    util

---
+++ Demo_4/util.js
@@ -0,0 +1,17 @@
+var Util = {};
+
+Util.getTimeStrFromUnix = function (time) {
+    time = parseInt(time * 1000)
+    if(isNaN(time)) {
+        return ''
+    }
+    var newDate = new Date(time)
+    var year = newDate.getFullYear()
+    var month = (newDate.getMonth() + 1) < 10 ? '0' + (newDate.getMonth() + 1) : newDate.getMonth() + 1
+    var day = newDate.getDate() < 10 ? '0' + newDate.getDate() : newDate.getDate()
+    var hours = newDate.getHours() < 10 ? '0' + newDate.getHours() : newDate.getHours()
+    var minuts = newDate.getMinutes() < 10 ? '0' + newDate.getMinutes() : newDate.getMinutes()
+    var seconds = newDate.getSeconds() < 10 ? '0' + newDate.getSeconds() : newDate.getSeconds()
+    var ret = year + '-' + month + '-' + day + ' ' + hours + ':' + minuts + ':' + seconds
+    return ret
+}
