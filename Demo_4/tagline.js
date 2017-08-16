d8a12650cd18f3a96a69398ba31478caaf565c52
linghuam <1551827898@qq.com>
2017-08-16 13:47:28 +0800

    折线图

--- Demo_4/tagline.js
+++ Demo_4/tagline.js
@@ -1,63 +1,94 @@
 class TagLine {
 
-  constructor(filename, elementId) {
+    constructor(filename, elementId, lineType) {
 
-    this._worldsData = null;
-    this._echarts = echarts.init(document.getElementById(elementId));
-    this.getData(filename).then(this._callbackFunc.bind(this));
-  }
-
-  async getData(filename) {
-    this._worldsData = await Util.readFile(filename);
-  }
-
-  _callbackFunc() {
-    var x = [],
-      y = [];
-    for(let i = 0, len = this._worldsData.length; i < len; i++) {
-      x.push(this._worldsData[i].tag_name);
-      y.push(parseInt(this._worldsData[i].tag_count));
+        this._allData = null;
+        this._lineType = lineType;
+        this._echarts = echarts.init(document.getElementById(elementId));
+        this.getData(filename).then(this._callbackFunc.bind(this));
     }
 
-    var option = {
-      title: {
-        text: '折线图'
-      },
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend: {
-        data: x
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      toolbox: {
-        feature: {
-          saveAsImage: {}
-        }
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: x
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-          name: '总量',
-          type: 'line',
-          data: y
-        }
-      ]
-    };
+    async getData(filename) {
+        this._allData = await Util.readFile(filename);
+    }
 
+    _callbackFunc() {
+        var filterData = this._allData.where(o => o.line_type === this._lineType);
+        var grby = filterData.groupBy('tag_name');
+        var series = [];
+        var legendData = [];
+        for(let i = 0, len = grby.length; i < len; i++) {
+            series.push({
+                name: grby[i].key,
+                type: 'line',
+                data: this.getseriesData(grby[i].value)
+            });
+            legendData.push(grby[i].key);
+        }
 
-    this._echarts.setOption(option);
-  }
+        var option = {
+            title: {
+                text: this._lineType,
+                left: 'left'
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter: function (params) {
+                    params = params[0];
+                    var name = params.name;
+                    var time = params.value[0];
+                    var count = params.value[1];
+                    return name + ':' + count + '\n' + time;
+                },
+                axisPointer: {
+                    animation: false
+                }
+            },
+            legend: {
+              data: legendData,
+              orient: 'horizontal',
+              left: 'center'
+            },
+            xAxis: {
+                type: 'time',
+                name: '时间',
+                axisTick: {
+                    show: true,
+                    alignWithLabel: false,
+                    interval: 0
+                },
+                axisLabel: {
+                    show: true,
+                    interval: 0,
+                    formatter: function (value) {
+                        return Util.getTimeStrFromUnix2(value);
+                    }
+                },
+                splitLine: {
+                    show: false
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: '个数',
+                splitLine: {
+                    show: true
+                }
+            },
+            series: series
+        };
+        this._echarts.setOption(option);
+    }
+
+    getseriesData(arr) {
+        var data = [];
+        for(let i = 0, len = arr.length; i < len; i++) {
+            data.push({
+                name: arr[i].tag_name,
+                value: [arr[i].create_time, parseInt(arr[i].tag_count)]
+            });
+        }
+        return data;
+    }
 
 }
--- Demo_4/util.js
+++ Demo_4/util.js
@@ -49,6 +49,22 @@ Util.getTimeStrFromUnix = function (time) {
   return ret
 }
 
+Util.getTimeStrFromUnix2 = function (time) {
+  time = parseInt(time)
+  if(isNaN(time)) {
+    return ''
+  }
+  var newDate = new Date(time)
+  var year = newDate.getFullYear()
+  var month = (newDate.getMonth() + 1) < 10 ? '0' + (newDate.getMonth() + 1) : newDate.getMonth() + 1
+  var day = newDate.getDate() < 10 ? '0' + newDate.getDate() : newDate.getDate()
+  var hours = newDate.getHours() < 10 ? '0' + newDate.getHours() : newDate.getHours()
+  var minuts = newDate.getMinutes() < 10 ? '0' + newDate.getMinutes() : newDate.getMinutes()
+  var seconds = newDate.getSeconds() < 10 ? '0' + newDate.getSeconds() : newDate.getSeconds()
+  var ret = year + '/' + month + '/' + day 
+  return ret
+}
+
 //得到特定格式(yy-mm-dd hh:mm:ss)时间的unix时间戳
 Util.getCusUnixTime = function (sjstr) {
   var sjsplit = sjstr.split(" ");
