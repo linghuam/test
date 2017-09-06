bc65eaf675b4f4686cc4935d8efc0061592e6afe
linghuam <1551827898@qq.com>
2017-09-06 21:38:27 +0800

    nodejsWebsocket

---
+++ nodejsWebsocket/websocket/ch1/server.js
@@ -0,0 +1,10 @@
+﻿// http://www.cnblogs.com/stoneniqiu/p/5402311.html
+// https://www.npmjs.com/package/nodejs-websocket
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({ port: 8181 });
+wss.on('connection', function (ws) {
+    console.log('client connected');
+    ws.on('message', function (message) {
+        console.log(message);
+    });
+});
