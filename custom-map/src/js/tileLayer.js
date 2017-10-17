2dc6db353e033019eb07e4b76c49c4de288870ac
linghuam <1551827898@qq.com>
2017-10-17 18:14:22 +0800

    c

--- custom-map/src/js/tileLayer.js
+++ custom-map/src/js/tileLayer.js
@@ -4,7 +4,7 @@ const DPI = 96;
 const IN2CM = 2.5399998;
 //切图级别对应的比例尺
 const LEVELSCALE = {
-	
+
 };
 export class TileLayer {
 
@@ -13,7 +13,7 @@ export class TileLayer {
     this.options = Object.assign({
       tileSize: 256
     }, options);
-    this._tileSize = this.options.tileSize; 
+    this._tileSize = this.options.tileSize;
   }
 
   onAdd(map) {
@@ -21,8 +21,39 @@ export class TileLayer {
     this._initContainer();
     this._tileCenter = this._map.project(this._map._center);
     this._tileZoom = this._map._zoom;
-    //当前级别下屏幕上1像素代表的实际距离
-    this._resolution = this.getResolution(this._tileZoom);
+    // 地图范围
+    var mapMinX = Math.round(this._tileCenter[0] - this._map._width / 2);
+    var mapMinY = Math.round(this._tileCenter[1] - this._map._height / 2);
+    var maxX = Math.round(this._tileCenter[0] + this._map._width / 2);
+    var maxY = Math.round(this._tileCenter[1] + this._map._height / 2);
+    // 该范围内切片行列范围
+    var rows = [Math.floor(mapMinX / this._tileSize), Math.floor(maxX / this._tileSize)];
+    var cols = [Math.floor(mapMinY / this._tileSize), Math.floor(maxY / this._tileSize)];
+    // 获取所有行列数据
+    var coords = [];
+    for(let i = rows[0]; i <= rows[1]; i++) {
+      for(let j = cols[0]; j <= cols[1]; j++) {
+        coords.push({
+          x: i,
+          y: j,
+          z: this._tileZoom
+        });
+      }
+    }
+    // 获取所有图片
+    for(let i = 0, len = coords.length; i < len; i++) {
+      let cor = coords[i];
+      let img = new Image();
+      img.onload = function () {
+        console.log(cor.x + cor.y + cor.z);
+      }
+      img.src = this.getTileUrl(cor);
+    }
+
+  }
+
+  getTileUrl(coords) {
+    return this._url.replace('{x}', coords.x).replace('{y}', coords.y).replace('{z}', coords.z)
   }
 
   onRemove(map) {
@@ -30,7 +61,7 @@ export class TileLayer {
   }
 
   getResolution(level) {
-  	return LEVELSCALE[level] * IN2CM / DPI;
+    return LEVELSCALE[level] * IN2CM / DPI; //当前级别下屏幕上1像素代表的实际距离
   }
 
   _initContainer() {
